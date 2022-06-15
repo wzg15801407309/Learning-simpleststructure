538d7e253e0d3dd73236e77fe77980635c5ac6d0
wzg15801407309 <1051084301@qq.com>
2022-06-15 15:17:08 +0800

    day2数据

--- 0ne-day-structure/0002-structure.js
+++ 0ne-day-structure/0002-structure.js
@@ -1,20 +1,48 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
- var removeDuplicates = function(nums) {
-  const len = nums.length;
-  if(len === 0) {return 0;};
-  let first = 1,next = 1;
-  while(first < len){
-      if(nums[first] !== nums[first-1]){
-          nums[next] = nums[first];
-          ++next;
-      }
-      ++first;
-  }
-  return nums.slice(0,next);
-};
+/** 旋转数组 */
+// 题解
+/** 给一个数组：[1,2,3,4,5,6,7,8,9,0],给一个k=3，
+ * 明白k的意义 k代表的将数组向右轮转 k 个位置，理解就是，把数组的最后k个位置挪到最前面
+ * 旋转数组变成：[8,9,0,1,2,3,4,5,6,7]
+*/
+/** my的思路 
+ * 根据k把数组分成两个数组，然后在contact链接在一起就好好了,如下：
+*/
+// const arrs = [1,2,3,4,5,6,7,8,9,0];
+// const rotateMy = (arrs,k)=>{
+//   let len = arrs.length;
+//   return arrs.slice(len-k).concat(arrs.slice(0,len-k))
+// };
+// const newArr =  rotateMy(arrs,3);
+// console.log(newArr);
+/** 我实现了，但是不符合题意，题意是原地改变数组，直接concat应该违反了这个本意吧 */
 
-const array = [1,1,2,2,3,4,4,5,5,6,6,7,8,8,9,9];
-console.log(removeDuplicates(array));
+// 官方的解答
+//方法一：使用额外的数组
+// const rotate_01 = (arrs,k)=>{
+//   let len = arrs.length;
+//   const newArr = new Array(len);
+//   arrs.forEach((num,i) => {
+//     newArr[(i+k)%len] = num;
+//   });
+//   newArr.forEach((num,i) => {
+//     arrs[i] = num
+//   });
+//   console.log(arrs);
+// }
+// rotate_01(arrs,3);
+
+// 方法二：我的想法
+// const arrs = [1,2,3,4,5,6,7,8,9,0];
+// const rotate_02 = (arrs,k)=>{
+//   let len = arrs.length;
+//   for (let i = 0; i < k; i++) {
+//     let temp =  arrs[len-1];
+//     for (let j = len-1; j>=0; j--) {
+//       arrs[j] = arrs[j-1]
+//     }
+//     arrs[0] = temp;
+//   }
+//   console.log(arrs);
+// }
+// // rotate_02(arrs,2);
+// console.log('-----------');
