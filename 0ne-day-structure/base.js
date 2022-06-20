e8841814745a29caab4f6a5c0008b07d73e9160a
wzg15801407309 <1051084301@qq.com>
2022-06-20 10:16:22 +0800

    基本js创建

---
+++ 0ne-day-structure/base.js
@@ -0,0 +1,10 @@
+/** 
+ * 题目：xxxxxxxx
+ */
+ console.log('************* me start ****************')
+ console.log('************* me end ******************')
+
+
+
+ console.log('************* normal start ****************')
+ console.log('************* normal end ******************')
