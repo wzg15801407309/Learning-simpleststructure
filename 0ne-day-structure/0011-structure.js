1b3f5bacc3bf6de5f207ab39345e5f011680bdf4
wzg15801407309 <1051084301@qq.com>
2022-06-30 18:04:08 +0800

    是否是有效的数独other

--- 0ne-day-structure/0011-structure.js
+++ 0ne-day-structure/0011-structure.js
@@ -16,7 +16,7 @@ const board =
   [".",".",".",".","8",".",".","7","9"]
 ]
 console.log('************* me start ****************')
-const isValidSudoku = (board) =>{
+const isValidSudokuMY = (board) =>{
   // 行的检测
   let row = 0;
   let list = 0;
@@ -50,10 +50,38 @@ const isValidSudoku = (board) =>{
   }
   return true;
 }
-console.log(isValidSudoku(board));
+console.log(isValidSudokuMY(board));
 console.log('************* me end ******************')
 
 
 
 console.log('************* normal start ****************')
+const isValidSudoku = board =>{
+  let len = board.length;
+  const rowSet = new Set()
+  const columnSet = new Set()
+  const setList = Array.from({ length: 9 }, () => new Set()); // 块？？
+  for (let i = 0; i < len; i++) {
+    for (let j = 0; j < len; j++) {
+      const m = parseInt(i / 3)
+      const n = parseInt(j / 3)
+      const idx = m * 3 + n // setList的索引 ??
+      if(j === 0){
+        rowSet.clear()
+        columnSet.clear()
+      };
+      if(rowSet.has(board[i][j]) || columnSet.has(board[j][j])) return false;
+      // 这个是不理解的 
+      if(setList[idx].has(board[i][j])) return false;
+      board[i][j] !='.'  && setList[idx].add(board[i][j])
+
+      board[i][j] !== '.' && rowSet.add(board[i][j])
+      board[j][i] !== '.' && columnSet.add(board[j][i])
+      console.log(idx,setList);
+    }
+  }
+  return true;
+}
+
+console.log(isValidSudoku(board));
 console.log('************* normal end ******************')
