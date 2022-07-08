4832aa8231cfbd85663358a7a711356e4966964e
wzg15801407309 <1051084301@qq.com>
2022-07-08 15:07:57 +0800

    整数反转

--- String-structure/02-string.js
+++ String-structure/02-string.js
@@ -2,6 +2,8 @@
  * 题目：整数旋转
  * 条件：如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
  * 
+ * 
+ * 知道的知识点：～x 相当于 ：x 进行按位非操作的结果为 -(x + 1)
  */
 
 console.log('************* me start ****************')
@@ -18,7 +20,13 @@ const myReverse = (x) =>{
     arrNew[i] = arrNew[len-i-1];
     arrNew[len-i-1] = temp;
   }
-  return Number(arrNew.join(''));
+  // 学习后 添加上的值溢出条件
+  const result =Number(arrNew.join(''))
+  if(result<Math.pow(-2,31)||result>Math.pow(2,31)-1){
+    return 0;
+  }else{
+    return result;   
+  }
 };
 console.log(myReverse(number));
 console.log('~~~自己的想发感觉还是想复杂了~~~~');
@@ -26,4 +34,21 @@ console.log('************* me end ******************')
 
 
 console.log('************* normal start ****************')
+// 数学的方式：～x 相当于 ：x 进行按位非操作的结果为 -(x + 1)
+const number1 = 9005000;
+const  mathReverse = (x)=>{
+  let rev = 0;
+    while (x !== 0) {
+        const digit = x % 10;
+        x = ~~(x / 10);//这个还不是特别明白  ～～(x/10) == parseInt(x/10) 前面的效率要好一些
+        rev = rev * 10 + digit;
+        console.log(x,rev);
+        if (rev < Math.pow(-2, 31) || rev > Math.pow(2, 31) - 1) {
+            return 0;
+        }
+    }
+    return rev;
+}
+console.log(mathReverse(number1));
+
 console.log('************* normal end ******************')
