f1c279a2ff1bce8ee735519d2291127d37322e82
wzg15801407309 <1051084301@qq.com>
2022-08-01 18:13:38 +0800

    回文链表的早起

---
+++ Linked-ist/05-link-list.js
@@ -0,0 +1,13 @@
+/** 
+ * 题目：回文链表 （对称和数组的一样）
+*/
+
+console.log('************* start ****************')
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+const isPalindrome = (head)=>{
+    console.log('回文链表');
+};
+console.log('************* end ******************')
