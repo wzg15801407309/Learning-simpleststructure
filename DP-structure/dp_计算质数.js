74d8873491694cf2bd68e27c1af4ba559ba2e7b7
wzg15801407309 <1051084301@qq.com>
2023-03-27 12:45:55 +0800

    20230327

--- DP-structure/dp_计算质数.js
+++ DP-structure/dp_计算质数.js
@@ -10,22 +10,38 @@
  * @return {number}
  */
 var countPrimes = function (n) {
-  // 创建了 长度为n的数组  
-  // 并让 假设所有的都为质数
-  const isPrime = new Array(n).fill(1);
-  console.log(isPrime);
-  let ans = 0;
-  // 明白：i代表的是当前的要判断的数字
-  for (let i = 2; i < n; ++i) {
-    if (isPrime[i]) {// 明白 if（0）/if（1）
-      ans += 1;
-      //  为什么是i*i 
-      for (let j = i * i; j < n; j += i) {
-        console.log(i, j);
-        isPrime[j] = 0;
+  // 在litcode上超时  
+  // // 创建了 长度为n的数组  
+  // // 并让 假设所有的都为质数
+  // const isPrime = new Array(n).fill(1);
+  // console.log(isPrime);
+  // let ans = 0;
+  // // 明白：i代表的是当前的要判断的数字
+  // for (let i = 2; i < n; ++i) {
+  //   if (isPrime[i]) {// 明白 if（0）/if（1）
+  //     ans += 1;
+  //     //  为什么是i*i 
+  //     for (let j = i * i; j < n; j += i) {
+  //       console.log(i, j);
+  //       isPrime[j] = 0;
+  //     }
+  //   }
+  // }
+  // return ans;
+
+  let count = n <= 2 ? 0 : 1
+  // Int8Array 创建n长度的整数数组
+  const isCom = new Int8Array(n)
+  for (let i = 3; i < n; i += 2) {
+    if (!isCom[i]) {
+      count++
+      if (i * i < n) {
+        for (let j = i; i * j < n; j += 2) {
+          isCom[i * j] = 1
+        }
       }
     }
   }
-  // return ans;
+  return count
 };
 console.log(countPrimes(20));
