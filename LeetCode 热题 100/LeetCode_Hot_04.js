1e5077d1fa32a0566aacb3bb48c46dcd4555b3cb
wzg15801407309 <1051084301@qq.com>
2023-10-17 09:41:57 +0800

    第四题

--- LeetCode 热题 100/LeetCode_Hot_04.js
+++ LeetCode 热题 100/LeetCode_Hot_04.js
@@ -1,6 +1,23 @@
 /**  LeetCode 热题 100 */
-/** 第二题
- * 题目：字母异位词分组
- * 题意：字母异位的分词指的是：单词的字母相同
- * 提示：仅包含小写字母
+/** 第四题
+ * 题目：给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+ * 题意：把所有的0移到末尾，非0元素排列顺序不变
+ * 提示：必须在不复制数组的情况下原地对数组进行操作
  */
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+let moveZeroes = (nums) => {
+  // 解法一 取巧
+  // nums.sort((a,b) => b? 0: -1)
+  // 双指针 
+  let slowIndex = 0;
+  for (let fastIndex = 0; fastIndex < nums.length; fastIndex++) {
+    if (nums[fastIndex] !== 0) {
+      [nums[slowIndex], nums[fastIndex]] = [nums[fastIndex], nums[slowIndex]];
+      slowIndex++;
+    }
+  }
+
+};
---
+++ LeetCode 热题 100/LeetCode_Hot_05.js
@@ -0,0 +1,6 @@
+/**  LeetCode 热题 100 */
+/** 第xxx题
+ * 题目：xxxxxxx
+ * 题意：xxxxxxx
+ * 提示：xxxxxxx
+ */
