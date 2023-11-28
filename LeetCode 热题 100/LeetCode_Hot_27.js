c7f193c7a9de2c173534602d9395a387be112ed4
wzg15801407309 <1051084301@qq.com>
2023-11-28 11:31:16 +0800

    20231128-27

---
+++ LeetCode 热题 100/LeetCode_Hot_27.js
@@ -0,0 +1,35 @@
+/**  LeetCode 热题 100 */
+/** 第27ß题
+ * 题目：合并两个有序链表
+ * 要求：将两个升序链表合并为一个新的 升序 链表并返回。新链表是通过拼接给定的两个链表的所有节点组成的。
+ * 方案名称：xxxxxxx
+ */
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+let mergeTwoLists = (list1, list2) => {
+  const prehead = new ListNode(-1);
+  let prev = prehead;
+  while (list1 != null && list2 != null) {
+    if (list1.val <= list2.val) {
+      prev.next = list1;
+      list1 = list1.next;
+    } else {
+      prev.next = list2;
+      list2 = list2.next;
+    }
+    prev = prev.next;
+  }
+  // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+  prev.next = list1 === null ? list2 : list1;
+  return prehead.next;
+};
