d85f83cec59cd9420625c5e00789ee8525be4236
wzg15801407309 <1051084301@qq.com>
2024-01-11 11:21:04 +0800

    20240111-52

---
+++ LeetCode 热题 100/LeetCode_Hot_52.js
@@ -0,0 +1,28 @@
+/**  LeetCode 热题 100 */
+/** 第53题
+ * 题目：搜索插入位置
+ * 题解：给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+ * 要求：请必须使用时间复杂度为 O(log n) 的算法。
+ * 方案名称：xxxx
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+const searchInsert = (nums, target) => {
+  const n = nums.length;
+  let left = 0;
+  let right = n - 1;
+  let ans = n;
+  while (left <= right) {
+    let mid = ((right - left) >> 1) + left;
+    if (target <= nums[mid]) {
+      ans = mid;
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return ans;
+};
--- LeetCode 热题 100/LeetCode_Hot_xx.js
+++ LeetCode 热题 100/LeetCode_Hot_xx.js
@@ -4,10 +4,3 @@
  * 题解：xxxxx
  * 方案名称：xxxx
  */
-/**
- * Definition for singly-linked list.
- * function ListNode(val, next) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.next = (next===undefined ? null : next)
- * }
- */
