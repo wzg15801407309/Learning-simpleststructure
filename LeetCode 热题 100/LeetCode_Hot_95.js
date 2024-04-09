6d6bdb8d8c9ad84095e17c8bef8d7093c5866eae
wzg15801407309 <1051084301@qq.com>
2024-04-09 17:51:36 +0800

    20240409-95

---
+++ LeetCode 热题 100/LeetCode_Hot_95.js
@@ -0,0 +1,28 @@
+/**  LeetCode 热题 100 */
+/** 第95题
+ * 题目：最长公共子序列
+ * 题解：给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ * 解释：一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+ * 方案名称：最长公共子序列问题是典型的二维【动态规划】问题。
+ */
+/**
+ * @param {string} text1
+ * @param {string} text2
+ * @return {number}
+ */
+const longestCommonSubsequence = (text1, text2) => {
+  const m = text1.length, n = text2.length;
+  const dp = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  for (let i = 1; i <= m; i++) {
+    const c1 = text1[i - 1];
+    for (let j = 1; j <= n; j++) {
+      const c2 = text2[j - 1];
+      if (c1 === c2) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+  return dp[m][n];
+};
