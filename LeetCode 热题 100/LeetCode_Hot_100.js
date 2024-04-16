d16fe8cc57b8083ba88f0b8bb31c6fa968ab2eb4
wzg15801407309 <1051084301@qq.com>
2024-04-16 10:04:59 +0800

    20240416-100

---
+++ LeetCode 热题 100/LeetCode_Hot_100.js
@@ -0,0 +1,13 @@
+/**  LeetCode 热题 100 */
+/** 第100题
+ * 题目：最长有效括号
+ * 题解：给你一个只包含 '(' 和 ')' 的字符串，找出最长有效（格式正确且连续）括号子串的长度。
+ * 方案名称：动态规划
+ */
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestValidParentheses = function (s) {
+
+};
